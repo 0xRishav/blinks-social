a2d03ee1aeb9c8f3e35a6dccc8e69fe1230e0f92
Rishav Bharti <rishav.bharti@omnycomm.com>
2023-04-06 02:10:02 +0530

    add fonts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import './index.css';
+import './fonts/Gilroy-Bold.ttf';
+import './fonts/Gilroy-ExtraBold.ttf';
+import './fonts/Gilroy-Heavy.ttf';
+import './fonts/Gilroy-Light.ttf';
+import './fonts/Gilroy-Medium.ttf';
+import './fonts/Gilroy-Regular.ttf';
+import './fonts/Gilroy-SemiBold.ttf';
+import './fonts/Gilroy-Thin.ttf';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
